df0104a3f6044c066ac57451528ec3c767652fdf
munhyunsu <munhyunsu@gmail.com>
2020-04-21 16:39:32 +0900

    sniff prn

--- 99-session_length.py
+++ 99-session_length.py
@@ -18,25 +18,39 @@ def read_pcap(root_dir, ext=('.pcap', '.pcapng')):
                     queue.append(entry.path)
 
 
+def packet_counter(counter):
+    def process_pkt(pkt):
+        if not pkt.haslayer('IP'):
+            return
+        ip = pkt['IP']
+        if not (ip.haslayer('TCP') or ip.haslayer('UDP')):
+            return
+        if ip.haslayer('TCP'):
+            l4 = 'TCP'
+        elif ip.haslayer('UDP'):
+            l4 = 'UDP'
+        counter[0] = counter[0] + 1
+    return process_pkt
+
+
 def main():
     root_path = os.path.abspath(os.path.expanduser(input('Root path: ').strip()))
     print(f'Root Path: {root_path}')
 
     session_length = dict()
-    mid_print = dict()
     cnt = 0
     for label, path in read_pcap(root_path):
-        pcap = rdpcap(path)
+        counter = [0]
+        sniff(offline=path, prn=packet_counter(counter), store=False)
         label_dict = session_length.get(label, dict())
-        slen = len(pcap)
-        label_dict[slen] = label_dict.get(slen, 0) + 1
+        label_dict[counter[0]] = label_dict.get(counter[0], 0) + 1
         session_length[label] = label_dict
         cnt = cnt + 1
-        mid_print[label] = mid_print.get(label, 0) + 1
-        if cnt%10000 == 0:
-            print(f'{cnt}: {mid_print}')
-    print(f'{cnt}: {mid_print}')
+        if cnt%1000 == 0:
+            print(f'{cnt}: {session_length.keys()}')
+    print()
 
+    print(session_length)
     with open('session_length.pickle', 'wb') as f:
         pickle.dump(session_length, f)
 
