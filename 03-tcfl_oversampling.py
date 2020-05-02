55007b9721f3cd58dc66e8238a15f740788caf9f
munhyunsu <munhyunsu@gmail.com>
2020-05-02 21:35:07 +0900

    move function def

--- 03-tcfl_oversampling.py
+++ 03-tcfl_oversampling.py
@@ -18,31 +18,6 @@ FLAGS = None
 _ = None
 
 
-def save_ckpt(state, metrics):
-    global FLAGS
-    keras_model = create_keras_model()
-    keras_model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-                        metrics=[tf.keras.metrics.CategoricalAccuracy()])
-    tff.learning.assign_weights_to_keras_model(keras_model, state.model)
-    keras_model.save(FLAGS.output)
-
-    with open(os.path.join(FLAGS.output, 'metrics.pickle'), 'wb') as f:
-        pickle.dump(metrics, f)
-
-
-def load_ckpt(state):
-    keras_model = tf.keras.models.load_model(FLAGS.output)
-    state = tff.learning.state_with_new_model_weights(
-              state,
-              trainable_weights=[v.numpy() for v in keras_model.trainable_weights],
-              non_trainable_weights=[v.numpy() for v in keras_model.non_trainable_weights])
-
-    with open(os.path.join(FLAGS.output, 'metrics.pickle'), 'rb') as f:
-        metrics = pickle.load(f)
-
-    return state, metrics
-
-
 def main():
     # Print Parameters
     print(f'Parsed: {FLAGS}')
@@ -163,6 +138,34 @@ def main():
                                outputs=[pred])
 
         return model
+
+
+    def save_ckpt(state, metrics):
+        keras_model = create_keras_model()
+        keras_model.compile(loss=tf.keras.losses.CategoricalCrossentropy(
+                                   from_logits=True),
+                            metrics=[tf.keras.metrics.CategoricalAccuracy()])
+        tff.learning.assign_weights_to_keras_model(keras_model, state.model)
+        keras_model.save(FLAGS.output)
+    
+        with open(os.path.join(FLAGS.output, 'metrics.pickle'), 'wb') as f:
+            pickle.dump(metrics, f)
+    
+    
+    def load_ckpt(state):
+        keras_model = tf.keras.models.load_model(FLAGS.output)
+        state = tff.learning.state_with_new_model_weights(
+                  state,
+                  trainable_weights=
+                    [v.numpy() for v in keras_model.trainable_weights],
+                  non_trainable_weights=
+                    [v.numpy() for v in keras_model.non_trainable_weights])
+    
+        with open(os.path.join(FLAGS.output, 'metrics.pickle'), 'rb') as f:
+            metrics = pickle.load(f)
+
+        return state, metrics
+
 
     def model_fn():
         keras_model = create_keras_model()
