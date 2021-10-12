ddea7b05fc4f34b1819e2ed17583e2f79e013b92
munhyunsu <munhyunsu@gmail.com>
2021-10-12 20:49:21 +0900

    update client example

---
+++ framework_grpc/greeter_client.py
@@ -0,0 +1,16 @@
+import grpc
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+        print(f'Greeter client received: {response.message}')
+        response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+        print(f'Greeter client received: {response.message}')
+
+
+if __name__ == '__main__':
+    run()
