f2b10340fc28b9807b3b657dda9fe5bc1878b939
munhyunsu <munhyunsu@gmail.com>
2021-10-23 20:11:42 +0900

    argument

--- framework_grpc/main_server.py
+++ framework_grpc/main_server.py
@@ -1,40 +1,64 @@
+import os
+import argparse
+import time
 import concurrent
+import io
+import pickle
 
 import grpc
+import numpy as np
 
 import federated_pb2
 import federated_pb2_grpc
 
+FLAGS = _ = None
+DEBUG = False
+STIME = time.time()
 
 class Server(federated_pb2_grpc.Manager):
     def GetModel(self, request, context):
+        reply = federated_pb2.ModelReply()
         print(f'Information: {request.information}')
-        if request.information == federated_pb2.ModelRequest.Information.VERSION:
-            
-            print('VERSION')
-        elif request.information == federated_pb2.ModelRequest.Information.PARAMETER:
+        if request.information == request.Information.VERSION:
+            reply.version = '1.3.2'
+        elif request.information == request.Information.PARAMETER:
+            reply.parameter = np.asarray([1, 2, [3, 4, 5]], dtype=object).tobytes()
             print('PARAMETER')
-        elif request.information == federated_pb2.ModelRequest.Information.ARCHITECTURE:
+        elif request.information == request.Information.ARCHITECTURE:
             print('ARCHITECTURE')
-        value = '1.3.2'
-        return federated_pb2.ModelReply(version=value)
+        return reply
 
 
-def serve():
+def main():
     server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
     federated_pb2_grpc.add_ManagerServicer_to_server(Server(), server)
-    server.add_insecure_port('[::]:50051')
-    print(f'Run server')
+    server.add_insecure_port(f'{FLAGS.address}:{FLAGS.port}')
     server.start()
-    print(f'Start server')
+    if DEBUG:
+        print(f'[{int(time.time()-STIME)}] Start server')
     try:
         server.wait_for_termination()
     except KeyboardInterrupt:
-        print('End server')
         server.wait_for_termination(1)
-    print(f'Terminate server')
+    if DEBUG:
+        print(f'[{int(time.time()-STIME)}] End server')
 
 
 if __name__ == '__main__':
-    serve()
+    root_path = os.path.abspath(__file__)
+    root_dir = os.path.dirname(root_path)
+    os.chdir(root_dir)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true',
+                        help='The present debug message')
+    parser.add_argument('--address', type=str, default=''
+                        help='The bind address')
+    parser.add_argument('--port', type=int, default=50000,
+                        help='The bind port number')
+
+    FLAGS, _ = parser.parse_known_args()
+    DEBUG = FLAGS.debug
+
+    main()
 
