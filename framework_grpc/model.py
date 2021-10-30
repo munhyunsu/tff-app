c804497a99ae079752bb16987426d00fb94dae33
munhyunsu <munhyunsu@gmail.com>
2021-10-31 07:32:50 +0900

    parent model update

--- framework_grpc/model.py
+++ framework_grpc/model.py
@@ -2,12 +2,17 @@ class Model():
     def __init__(self, **kwargs):
         raise NotImplementedError
 
+    def get_name(self):
+        raise NotImplementedError
+
+    def get_compile(self):
+        raise NotImplementedError
+
+    def get_label(self):
+        raise NotImplementedError
+
+    def get_architecture(self):
+        raise NotImplementedError
+
     def get_parameter(self):
         raise NotImplementedError
-
-    def get_model(self):
-        raise NotImplementedError
-
-    def feed_train_result(self, result):
-        raise NotImplementedError
-
