67ecac926e07051f9c37ce65975ef1f34250d755
munhyunsu <munhyunsu@gmail.com>
2021-11-16 14:26:26 +0900

    Default activity

---
+++ CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.lunaticharmony.crowdim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
