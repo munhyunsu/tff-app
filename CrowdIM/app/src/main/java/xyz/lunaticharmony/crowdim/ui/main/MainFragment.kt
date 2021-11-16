075ba8343934c5c72f14fb62319ec206db2a9897
munhyunsu <munhyunsu@gmail.com>
2021-11-16 14:42:42 +0900

    fragment

---
+++ CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package xyz.lunaticharmony.crowdim.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import xyz.lunaticharmony.crowdim.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+}
