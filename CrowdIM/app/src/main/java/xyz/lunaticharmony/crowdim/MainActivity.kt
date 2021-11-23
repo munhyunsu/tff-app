6194e8ecc79cbe39e2ab6fe38025b0ee05a38afb
munhyunsu <munhyunsu@gmail.com>
2021-11-23 22:04:58 +0900

    button

--- CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/MainActivity.kt
+++ CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/MainActivity.kt
@@ -2,17 +2,19 @@ package xyz.lunaticharmony.crowdim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import xyz.lunaticharmony.crowdim.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, MainFragment.newInstance())
-//                .commitNow()
-//        }
+    }
+
+//    https://stackoverflow.com/questions/44301301/android-how-to-achieve-setonclicklistener-in-kotlin
+    fun checkButton(view: View) {
+        Toast.makeText(view.context, "your toast text", Toast.LENGTH_SHORT).show();
     }
 }
 
--- CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/ui/main/MainFragment.kt
+++ CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/ui/main/MainFragment.kt
@@ -5,12 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import xyz.lunaticharmony.crowdim.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
