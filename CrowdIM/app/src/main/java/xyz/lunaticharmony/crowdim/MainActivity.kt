cca12b5a9561974430cb8699411a33f237f5690a
munhyunsu <munhyunsu@gmail.com>
2021-11-23 09:42:11 +0900

    button

--- CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/MainActivity.kt
+++ CrowdIM/app/src/main/java/xyz/lunaticharmony/crowdim/MainActivity.kt
@@ -8,10 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
     }
 }
+
