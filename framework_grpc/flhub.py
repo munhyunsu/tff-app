ed89f0057923e9ef03802eeac1dc1851632d0e3d
munhyunsu <munhyunsu@gmail.com>
2022-01-04 23:17:46 +0900

    excutable program

---
+++ framework_grpc/flhub.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+
+FLAGS = _ = None
+DEBUG = False
+
+
+def main():
+    if DEBUG:
+        print(f'Parsed arguments {FLAGS}')
+        print(f'Unparsed arguments {_}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_arguments('--debug', action='store_true',
+                         help='The present debug message')
+
+    FLAGS, _ = parser.parse_known_args()
+    DEBUG = FLAGS.debug
+
+    main()
+
