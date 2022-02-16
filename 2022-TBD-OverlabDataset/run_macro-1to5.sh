b65e96a058da407a52d22803eeef54ff2622c6dd
munhyunsu <munhyunsu@gmail.com>
2022-02-17 08:32:11 +0900

    execute all

---
+++ 2022-TBD-OverlabDataset/run_macro-1to5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+time python3 run_macro-1.py > run_macro2-1.out.1
+time python3 run_macro-2.py > run_macro2-2.out.1
+time python3 run_macro-3.py > run_macro2-3.out.1
+time python3 run_macro-4.py > run_macro2-4.out.1
+time python3 run_macro-5.py > run_macro2-5.out.1
